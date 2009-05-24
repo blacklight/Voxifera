18cd96378df762bd588bb7c4110f8b1e49c28ce8
evilsocket <evilsocket@gmail.com>
2009-05-24 17:25:50 +0200

    Added gpl license comment to each file

--- main.c
+++ main.c
@@ -1,11 +1,29 @@
+/***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
 #include "vocal.h"
 
 typedef enum  { capture, append } mode;
 
 void helpandusage()  {
 	fprintf (stderr,
-			"---=== BlackLight's vocal recognition v.0.1b ===---\n"
-			"author @ blacklight@autistici.org\n\n"
+			"---=== BlackLight's && evilsocket's vocal recognition v.0.1b ===---\n"
+			"author @ blacklight@autistici.org\n"
+			"author @ evilsocket@gmail.com\n\n"
 			"Usage: vocal [-d <device>] [-a] [-c <file>] [-h]\n\n"
 			"\t-d <device>\tUse a different device instead the default one (/dev/dsp)\n"
 			"\t-a\t\tAppend a new vocal sample with an associated command\n"
